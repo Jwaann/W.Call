aba4dd566ce8bb66a09c56b55c1d9d600af97bee
Jwaann <jwanabdo.20@gmail.com>
2026-04-16 15:16:46 +0200

    Add TURN server for ICE connectivity - fixes WebRTC calls when direct P2P fails

--- client/src/hooks/usePeerConnection.js
+++ client/src/hooks/usePeerConnection.js
@@ -4,6 +4,11 @@ const RTC_CONFIG = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
+    {
+      urls: ['turn:turn.bistri.cc:80', 'turn:turn.bistri.cc:443'],
+      credential: 'homesweet',
+      username: 'homesweethome',
+    },
   ],
 };
 
